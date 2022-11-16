6e2acf993af03cbc5792b0e11daa2a07bc51c04d
Alfredo Acosta <alfredo.acosta@outlook.com>
2022-11-15 22:04:28 -0300

    Agregado de Models y se modificó views.py y turnos.html

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=30, verbose_name = "Nombre")
+    apellido = models.CharField(max_length=30, verbose_name = "Apellido")
+    celular = models.CharField(max_length=10)
+	
+class Turno(models.Model):
+	fecha = models.DateField()
+	hora = models.IntegerField()
+	estado = models.BooleanField(default = False)
+	paciente = models.ForeignKey(Paciente, on_delete = models.CASCADE)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
+from datetime import datetime, date, timedelta
+from mainapp.models import Paciente, Turno
 
 # Create your views here.
 def home(request):
     return render(request, "mainapp/home.html")
 
 def turnos(request):
-    return render(request, "mainapp/turnos.html")
+    turno_inicial = 16 # Horario del primer turno
+    horario = []
+    for n in range(0, 10): # 10 turnos de 30 minutos cada uno
+        agregar = str(turno_inicial+n//2)+":"+str(30*(n%2))
+        if len(agregar)==4:
+            agregar = agregar+"0"
+        horario.append(agregar)
+    
+    hoy = date.today()
+    dia = date.weekday(hoy)
+    meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    fecha = []
+    if dia > 4:
+        dia_inicial = 7 - dia
+        fecha_ini = hoy + timedelta(days= dia_inicial)
+    else:
+        dia_inicial = dia
+        fecha_ini = hoy - timedelta(days= dia_inicial)
+    for n in range(0, 5):
+        dia_imprimir = fecha_ini+timedelta(days=n)
+        fecha.append(dia_imprimir.day)
+    
+    turno_actual = Turno.objects.all()
+    
+    return render(request, "mainapp/turnos.html", {'horarios': horario, 'fechas': fecha, 'mes_turno': meses[fecha_ini.month], 'turnos': turno_actual, 'dias': [1,2,3,4,5]})
 
 def registro(request):
+    
     return render(request, "mainapp/registro.html")
 
 def contact(request):
